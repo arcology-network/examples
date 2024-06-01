b340c18bdbe00b02549cf9f3162dc3bc3f5aaaa3
sameplacewei <sameplace@126.com>
2024-06-01 11:30:19 +0800

    update network name

--- eth-examples/hardhat.config.js
+++ eth-examples/hardhat.config.js
@@ -2,26 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 require('dotenv').config()
 const nets = require('./network.json');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-// const words = process.env.MNEMONIC.match(/[a-zA-Z]+/g).length
-// validLength = [12, 15, 18, 24]
-// if (!validLength.includes(words)) {
-//    console.log(`The mnemonic (${process.env.MNEMONIC}) is the wrong number of words`)
-//    process.exit(-1)
-// }
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
