9d21506f783878febced2aea823d458a32aac73e
sameplacewei <sameplace@126.com>
2024-06-02 14:30:25 +0800

    add test case

---
+++ simple/test/test-like.js
@@ -0,0 +1,38 @@
+const hre = require("hardhat");
+var frontendUtil = require('@arcologynetwork/frontend-util/utils/util') 
+
+async function main() {
+    accounts = await ethers.getSigners(); 
+
+    const bt_factory = await ethers.getContractFactory("Like");
+    const bt = await bt_factory.deploy();
+    await bt.deployed();
+    console.log(`Deployed Like Test at ${bt.address}`)
+
+    console.log('===========like=====================')
+    var txs=new Array();
+    for(i=1;i<=10;i++){
+      txs.push(frontendUtil.generateTx(function([bt,from]){
+        return bt.connect(from).like();
+      },bt,accounts[i]));
+    }
+    await frontendUtil.waitingTxs(txs);
+    
+    console.log('===========getLikes=====================')
+    tx = await bt.getLikes();
+    const receipt=await tx.wait();
+    frontendUtil.showResult(frontendUtil.parseReceipt(receipt));
+    console.log(frontendUtil.parseEvent(receipt,"CounterQuery"))
+    if(frontendUtil.parseEvent(receipt,"CounterQuery")==="0x000000000000000000000000000000000000000000000000000000000000000a"){
+      console.log('Test Successful');
+    }else{
+      console.log('Test Failed');
+    } 
+  }
+
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
