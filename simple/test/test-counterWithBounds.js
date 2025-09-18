5abafdea114a4da44f79dad319fa1371dfbe33bd
sameplacewei <sameplace@126.com>
2025-09-18 20:09:34 +0800

    reorg

---
+++ simple/test/test-counterWithBounds.js
@@ -0,0 +1,56 @@
+const hre = require("hardhat");
+var frontendUtil = require('@arcologynetwork/frontend-util/utils/util')
+const nets = require('../network.json');
+
+async function main() {
+    accounts = await ethers.getSigners(); 
+
+    console.log('======start deploying contract======')
+    const counter_factory = await ethers.getContractFactory("CounterWithBounds");
+    const counter = await counter_factory.deploy();
+    await counter.deployed();
+    console.log(`Deployed Counter Test at ${counter.address}`)
+    
+    var txs=new Array();
+    let i,tx,receipt;
+   
+    console.log('======start executing TXs calling add======')
+    tx = await counter.add(10);
+    receipt=await tx.wait();
+    frontendUtil.showResult(frontendUtil.parseReceipt(receipt));
+    console.log(frontendUtil.parseEvent(receipt,"QueryBalance"))
+    if(frontendUtil.parseEvent(receipt,"QueryBalance")==="0x000000000000000000000000000000000000000000000000000000000000000a"){
+      console.log('Test Successful');
+    }else{
+      console.log('Test Failed');
+    }
+
+    console.log('======start executing TXs calling sub======')
+    tx = await counter.sub(5);
+    receipt=await tx.wait();
+    frontendUtil.showResult(frontendUtil.parseReceipt(receipt));
+    console.log(frontendUtil.parseEvent(receipt,"QueryBalance"))
+    if(frontendUtil.parseEvent(receipt,"QueryBalance")==="0x0000000000000000000000000000000000000000000000000000000000000005"){
+      console.log('Test Successful');
+    }else{
+      console.log('Test Failed');
+    }
+
+    console.log('======start executing TXs calling reset======')
+    tx = await counter.reset();
+    receipt=await tx.wait();
+    frontendUtil.showResult(frontendUtil.parseReceipt(receipt));
+    console.log(frontendUtil.parseEvent(receipt,"QueryBalance"))
+    if(frontendUtil.parseEvent(receipt,"QueryBalance")==="0x0000000000000000000000000000000000000000000000000000000000000000"){
+      console.log('Test Successful');
+    }else{
+      console.log('Test Failed');
+    }
+}
+
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
---
+++ simple/test/test-visitu256.js
@@ -0,0 +1,39 @@
+const hre = require("hardhat");
+var frontendUtil = require('@arcologynetwork/frontend-util/utils/util') 
+
+async function main() {
+    accounts = await ethers.getSigners(); 
+
+    console.log('======start deploying contract======')
+    const bt_factory = await ethers.getContractFactory("VisitsU256");
+    const bt = await bt_factory.deploy();
+    await bt.deployed();
+    console.log(`Deployed Bool Test at ${bt.address}`)
+
+    console.log('======start executing TXs calling visit======')
+    var txs=new Array();
+    for(i=1;i<=10;i++){
+      txs.push(frontendUtil.generateTx(function([bt,from]){
+        return bt.connect(from).visit();
+      },bt,accounts[i]));
+    }
+    await frontendUtil.waitingTxs(txs);
+    
+    console.log('======start executing TXs calling getCounter======')
+    tx = await bt.getCounter();
+    const receipt=await tx.wait();
+    frontendUtil.showResult(frontendUtil.parseReceipt(receipt));
+    console.log(frontendUtil.parseEvent(receipt,"CounterQuery"))
+    if(frontendUtil.parseEvent(receipt,"CounterQuery")==="0x000000000000000000000000000000000000000000000000000000000000000a"){
+      console.log('Test Successful');
+    }else{
+      console.log('Test Failed');
+    } 
+  }
+
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
